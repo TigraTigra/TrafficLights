220d35c34e90da29f0a03140990e89a688968546
TigraTigra <pavelremarque@gmail.com>
2023-04-30 13:38:31 +0300

    Setup logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -2,18 +2,61 @@
 //  ViewController.swift
 //  TrafficLights
 //
-//  Created by DC on 30.04.2023.
+//  Created by Pavel Mazurenko on 30.04.2023.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var redSignal: UIView!
+    @IBOutlet weak var yellowSignal: UIView!
+    @IBOutlet weak var greenSignal: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentSignal = CurrentSignal.red
+    private let signalOn: CGFloat = 1
+    private let signalOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        
+        redSignal.alpha = signalOff
+        yellowSignal.alpha = signalOff
+        greenSignal.alpha = signalOff
+        
+        redSignal.layer.cornerRadius = redSignal.frame.width / 2
+        yellowSignal.layer.cornerRadius = yellowSignal.frame.width / 2
+        greenSignal.layer.cornerRadius = greenSignal.frame.width / 2
+        
     }
 
-
+    @IBAction func startButtonTapped() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        switch currentSignal {
+        case .red:
+            greenSignal.alpha = signalOff
+            redSignal.alpha = signalOn
+            currentSignal = .yellow
+        case .yellow:
+            redSignal.alpha = signalOff
+            yellowSignal.alpha = signalOn
+            currentSignal = .green
+        case .green:
+            greenSignal.alpha = signalOn
+            yellowSignal.alpha = signalOff
+            currentSignal = .red
+        }
+    }
 }
-
+// MARK: - CurrentSignal
+extension ViewController {
+    private enum CurrentSignal {
+        case red, yellow, green
+    }
+}
